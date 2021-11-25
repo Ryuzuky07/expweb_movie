adbca04b3ce347180873c8622147ea0cbdafdfe8
Ryuzuky07 <bomartadinata2@gmail.com>
2021-11-25 19:27:23 +0800

    create server

---
+++ exp_movie.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const path = require('path');
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+
+app.get('/', (req, res) => {
+    console.log(__dirname)
+  res.sendFile(path.join())
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}/home`)
+  })
