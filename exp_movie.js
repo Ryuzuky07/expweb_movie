2b3a9e7c405e5bec84aad2887f90a32062be94de
I Ketut Gede Mahendra Saputra <48759474+MahendraSaputraa@users.noreply.github.com>
2021-11-27 22:46:27 +0800

    new folder placement
    seterah

--- exp_movie.js
+++ exp_movie.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 2000
 const path = require('path');
 
-app.get('/', (req, res) => {
-    console.log(__dirname)
-  res.sendFile(path.join(""))
+
+
+app.use('/assets', express.static('assets'))
+
+app.get('/home', (req, res) => {
+  console.log(__dirname)
+res.sendFile(path.join('D:/Github/expweb_movie/HTML/index.html'))
 })
 
 app.get('/', (req, res) => {
